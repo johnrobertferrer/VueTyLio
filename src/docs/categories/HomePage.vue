157397f793238f2143d8b222c1f17429a79b2141
John Ferrer <johnrobert.ferrer.nelsoft@gmail.com>
2020-04-30 00:39:41 +0800

    :star: Feat: Disabled highlight and grab photos

--- src/docs/categories/HomePage.vue
+++ src/docs/categories/HomePage.vue
@@ -650,6 +650,20 @@
         vertical-align:top;
     }
 
+    img {
+        pointer-events: none;
+    }
+
+    * { 
+        -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Safari */
+            -khtml-user-select: none; /* Konqueror HTML */
+                -moz-user-select: none; /* Old versions of Firefox */
+                -ms-user-select: none; /* Internet Explorer/Edge */
+                    user-select: none; /* Non-prefixed version, currently
+                                        supported by Chrome, Opera and Firefox */
+    }
+
     .bg-pattern {
         /* e25247 */
         background: linear-gradient(90deg, #e25247 20px, transparent 10%) center, linear-gradient(#e25247 20px, transparent 10%) center, #ebad04;
