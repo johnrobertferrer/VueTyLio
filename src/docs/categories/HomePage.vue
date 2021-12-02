a030e9fb719d074803d238fe5965dd6043e744bc
DESKTOP-KDJMVUO\John Ferrer <john.robert.s.ferrer@gmail.com>
2021-12-02 22:15:22 +0800

    :fire: Removed unable to select text
    Signed-off-by: DESKTOP-KDJMVUO\John Ferrer <john.robert.s.ferrer@gmail.com>

--- src/docs/categories/HomePage.vue
+++ src/docs/categories/HomePage.vue
@@ -683,16 +683,6 @@
         pointer-events: none;
     }
 
-    * { 
-        -webkit-touch-callout: none; /* iOS Safari */
-            -webkit-user-select: none; /* Safari */
-            -khtml-user-select: none; /* Konqueror HTML */
-                -moz-user-select: none; /* Old versions of Firefox */
-                -ms-user-select: none; /* Internet Explorer/Edge */
-                    user-select: none; /* Non-prefixed version, currently
-                                        supported by Chrome, Opera and Firefox */
-    }
-
     .bg-pattern {
         /* e25247 */
         background: linear-gradient(90deg, #e25247 20px, transparent 10%) center, linear-gradient(#e25247 20px, transparent 10%) center, #ebad04;
@@ -723,6 +713,7 @@
         word-break: break-word;
         font-size: 2rem;
         font-weight: bold;
+        margin-bottom: 0px;
     }
 
     .delay-1s {
